ce0a73614f4c1442828d823e5df5a4eac81c901e
Imrul Kayes <snowflake.imrul@gmail.com>
2025-11-21 05:50:18 +0000

    station_dimension model created

---
+++ models/station_dimension.sql
@@ -0,0 +1,15 @@
+WITH BIKE as(
+
+SELECT
+    DISTINCT
+    START_STATIO_ID as station_id,
+    start_station_name,
+    start_lat,
+    start_lng
+FROM {{ source('demo', 'bike') }}
+WHERE RIDE_ID != 'ride_id'
+)
+
+SELECT
+* 
+FROM BIKE
